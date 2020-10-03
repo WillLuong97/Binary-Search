789519854c1f5e30cb779d831e98fe2af06be88d
Will Luong <tluong@stedwards.edu>
2020-10-02 19:56:33 -0500

    Create searchInSortedRotatedArray.py

---
+++ searchInSortedRotatedArray.py
@@ -0,0 +1,48 @@
+#Python3 implementation of leetcode 81. Search in Rotated Sorted Array I and II using Binary search
+
+#Problem statement: 
+#search in a sorted rotated array I
+'''
+You are given an integer array nums sorted in ascending order, and an integer target.
+
+Suppose that nums is rotated at some pivot unknown to you beforehand (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+
+If target is found in the array return its index, otherwise, return -1.
+
+ 
+
+Example 1:
+
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+Example 2:
+
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1
+Example 3:
+
+Input: nums = [1], target = 0
+Output: -1
+
+'''
+#Function to find the target in a sorted rotated array: 
+def search_I(self, nums, target):
+	pass
+
+#Function to find the target in a sorted array with duplicate: 
+def search__II(nums, target):
+	pass
+
+#Driver code to execute the two main function: 
+def main():
+	print("TESTING SEARCHING IN ROTATED SORTED ARRAY I...")
+	test_nums = [4,5,6,7,0,1,2]
+	test_target = 0
+
+	test_nums_01 = [4,5,6,7,0,1,2]
+	test_target = 3
+
+	print()
+		
+	print("END OF TESTING..."
+main()
