2a89774f7cd3861501bd94832f56709c134b4d17
Will Luong <tluong@stedwards.edu>
2021-04-28 14:28:11 -0500

    Python3 implementation of comparing the string based on the frequency using linear approach

--- compareStringByFrequencyOfTheSmallestCharacter.py
+++ compareStringByFrequencyOfTheSmallestCharacter.py
@@ -28,41 +28,34 @@ Constraints:
 1 <= queries[i].length, words[i].length <= 10
 queries[i][j], words[i][j] consist of lowercase English letters.
 
+
+Approach: I am going to solve this problem via a naive and linear approach: 
+- Create a function f to calculate the frequency of the smallest lexicographical word in each words contains in both the queries and words array. 
+-Afterward, iterate through each array and compare them together, if the f(queries[i]) < f(words[i]) => increment the counter by 1
+
+Time complexity: O(n), the algorithm will have to look through all element in  both the arrays, calculate the frequency of each word conatin in both the array, then compare their frequency together. 
+Space complexity: O(n), we have to store two array of lexico smallest word in both the queries and the words
+
 '''
-import bisect
-from collections import Counter
 #Approach: we will find the smallest frequency of each words in each array, and once we have found them, we will comparing the two length together
 def numSmallerByFrequency(queries, words):
-	#base case:
-	if not queries or not words:
-		return None
-	answer = []
-	smallestWordFreqArr = []
-	#loop through each array to count its word frequency
-	for word in words:
-		smallestFreqInWord = helper(word)
-		smallestWordFreqArr.append(smallestFreqInWord)
-	smallestWordFreqArr.sort()
-	for query in queries: 
-		smallestFreqInQuery = helper(query)
-		insertionPoint = bisect.bisect_left(smallestWordFreqArr, smallestFreqInQuery)
-		wordCount = len(smallestWordFreqArr) - insertionPoint
-		answer.append(wordCount)
-	return answer
+	#create two arrays that store the frequencies of each smallest lexicograhical order of each string contains in both queries and words
+	fqueries = [f(query) for query in queries]
+	fwords = [f(word) for word in words]
+	result = []
+	#iterate through both the frequency array 
+	for q in fqueries:
+		#counter to count the number of word in words that are larger than queries in term of frequency
+		counter = 0
+		for w  in fwords:
+			if q < w: 
+				counter += 1
+		result.append(counter)
+	return result		
 
-#helper method to count the frequency of each words in each srting within each array
-def helper(word):
-	if not word:
-		return
-	#start counting the frequency in each letter:  
-	counter = Counter(word)
-	#getting the keys of each word
-	keys = sorted(counter.keys())
-	#we return the word with the smallest frequency
-	return counter[keys[0]]	
-
-
-	
+#helper method to return the frequency of the string with the smallest lexicographical order. 
+def f(string):
+	return string.count(min(string))
 #Main function to run the test cases:
 def main():
 	print("TESITNG COMPARE STRING BY FREQUENCY OF THE SMALLEST CHARACTERS...")
